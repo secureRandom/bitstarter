ca0c1544dd5b778326e3162f5739531ba08933c9
secureRandom <harrison.wood@gmail.com>
2013-07-14 18:49:24 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(256);
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
