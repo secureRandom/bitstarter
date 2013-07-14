23b6ecbc33b2a5ef21c55375257e6f0a6f14b724
secureRandom <harrison.wood@gmail.com>
2013-07-14 18:27:52 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-//var note = Buffer(fs.readFileSync('index.html', 'utf-8'));
-//var text = note.toString('utf-8', 0, 27);
+var note = Buffer(fs.readFileSync('.\index.html', 'utf-8'));
+var text = note.toString('utf-8', 0, 27);
 
 app.get('/', function(request, response) {
-  response.send("working");
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
