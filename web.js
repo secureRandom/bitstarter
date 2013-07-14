5251d5e221cfdf540a90c4e87aebc093067110cd
secureRandom <harrison.wood@gmail.com>
2013-07-14 02:12:44 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+note = Buffer(fs.readFileSync('index.html', 'utf-8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(note.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
