b93f53951b3ac38f4d396b9deca8e74f785eaac4
secureRandom <harrison.wood@gmail.com>
2013-07-14 18:46:29 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString(fs.readFileSync('index.html'), 'utf8'));
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
