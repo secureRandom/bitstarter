b0969cbe557d488fadcd1dbf3c1883a3f5afef16
secureRandom <harrison.wood@gmail.com>
2013-07-14 02:36:13 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var note = Buffer(fs.readFileSync('index.html', 'utf-8'));
-text = note.toString('utf-8');
+var text = note.toString('utf-8', 0, 27);
 
 app.get('/', function(request, response) {
   response.send(text);
